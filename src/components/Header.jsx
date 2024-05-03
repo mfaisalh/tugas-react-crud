da5dcd7cde6f49d3566d2a2a6874cc80905646a4
Muhamad Faisal Hidayatullah <mfaisalh140202@gmail.com>
2024-05-03 11:16:39 +0700

    fix: button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,16 @@ const Header = () => {
   return (
     <>
       <div>
-        <nav className="navbar navbar-expand-lg bg-dark justify-content-center" data-bs-theme="dark">
+        <nav
+          className="navbar navbar-expand-lg bg-dark justify-content-center"
+          data-bs-theme="dark"
+        >
           <div className="container my-2">
             <Link to="/" className="navbar-brand">
               HOME
             </Link>
             <Link
+              id="tombolPost"
               to="/post"
               className="btn btn-success py-2 px-4 nav-link active"
               aria-current="page"
